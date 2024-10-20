9f287ab355c1349f17b8d0cacb9b3e2ccc3a2566
freddiee <freddie.ergatoudis@mkodo.com>
2024-10-20 19:06:35 +0100

    Home Page margin adjustment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,12 +13,12 @@ const HomePage = ({ carousel }: HomePageProps): React.JSX.Element => {
   const img = carousel[Math.floor(Math.random() * carousel.length)];
 
   return (
-    <Container fluid className="d-flex" style={{ marginBottom: "15px" }}>
+    <Container fluid className="d-flex">
       <div className="d-flex flex-column flex-grow-1 bg-image position-relative overflow-hidden">
         <div className="position-relative flex-grow-1 flex-shrink-0 rounded overflow-hidden">
           <Image src={process.env.NEXT_PUBLIC_CMS_URL + img.attributes.url} alt="cover" fill objectFit="cover" />
         </div>
-        <div className="text-center mt-auto" style={{ fontSize: "1.5rem" }}>
+        <div className="text-center mt-3" style={{ fontSize: "1.5rem" }}>
           Interior Design · Architecture · Construction · Planning
         </div>
       </div>
