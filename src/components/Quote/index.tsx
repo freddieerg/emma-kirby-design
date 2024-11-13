baf2dc26ed08b1326af1e0ea1cd04bd3d56480ff
freddiee <freddie.ergatoudis@mkodo.com>
2024-11-13 12:02:22 +0000

    Sizing adjustments

--- src/components/Quote/index.tsx
+++ src/components/Quote/index.tsx
@@ -7,7 +7,7 @@ interface QuoteProps {
 export default function Quote({ quote, author, subtitle }: QuoteProps) {
     return (
         <div className={"text-center p-12 sm:p-20"}>
-            <div className={"text-lg sm:text-2xl"}>&#34;{quote}&#34;</div>
+            <div className={"text-lg sm:text-xl"}>&#34;{quote}&#34;</div>
             <div className={"mt-12 font-bold"}>{author}</div>
             <div className={"opacity-70"}>{subtitle}</div>
         </div>
