0ad8700956dfe30829704dd15d815f29ecad90f3
freddiee <freddie.ergatoudis@mkodo.com>
2025-03-24 12:45:46 +0000

    Add a 1 year cache to images

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'pub-243033a27b3d4f2688a28673503147e9.r2.dev',
-      },
-    ],
-  },
+    images: {
+        minimumCacheTTL: 365 * 24 * 60 * 60, // 1 year
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "pub-243033a27b3d4f2688a28673503147e9.r2.dev",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
